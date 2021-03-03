8dafa804d9af6c71111b803dd4f053269ee6145c
Jiachenn Choong <choongjiachenn@gmail.com>
2021-03-03 14:35:19 +0000

    Added pretrained, no pretrained results

--- run_experiments_3d.sh
+++ run_experiments_3d.sh
@@ -12,16 +12,13 @@ do
 
 echo "Current i(seed) in script:" $i
 
-#python3 train_jonas_net_batch.py -name brats20_3d_pre_1_lol --batch_size 10 --patch_depth 24 --brats_train_year 20 --brats_test_year 20 --epochs 50 --multi_gpu --seed 1
-
 # Multi gpu
 python3 train_jonas_net_batch.py -name brats20_3d_pre_$i --batch_size 10 --patch_depth 24 --brats_train_year 20 --brats_test_year 20 --epochs 50 --multi_gpu --seed $i
+python3 train_jonas_net_batch.py -name brats20_3d_no_pretrained_$i --batch_size 10 --patch_depth 24 --brats_train_year 20 --brats_test_year 20 --epochs 50 --multi_gpu --no_pretrained --seed $i
 
 # Multi gpu testing
 #python3 train_jonas_net_batch.py -name brats20_3d_pre_$i --batch_size 10 --patch_depth 24 --brats_train_year 20 --brats_test_year 20 --epochs 10 --multi_gpu --seed $i
 
 # No multi gpu testing
-#python3 train_jonas_net_batch.py -name brats20_3d_pre_$i --batch_size 10 --patch_depth 24 --brats_train_year 20 --brats_test_year 20 --epochs 2 --seed $i
-
-#python3 train_jonas_net_batch.py -name brats20_3d_1 --batch_size 10 --patch_depth 24 --brats_train_year 20 --brats_test_year 20 --no_pretrained --seed 1 --epochs 2
+#python3 train_jonas_net_batch.py -name brats20_3d_pre_$i --batch_size 10 --patch_depth 24 --brats_train_year 20 --brats_test_year 20 --epochs 2 --seed $i brats20_3d_pre_1_lr_0.001_epochs_50
 done
