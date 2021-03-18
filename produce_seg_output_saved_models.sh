a4dfaca93bcb777c2676295e3b42e0a664bf53e8
Jiachenn Choong <choongjiachenn@gmail.com>
2021-03-18 13:42:55 +0000

    Current latest changes

--- produce_seg_output_saved_models.sh
+++ produce_seg_output_saved_models.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
 #SBATCH -p cs -A cs -q csug
-#SBATCH -c12 --mem=24g
-#SBATCH --gres gpu:2 --constraint=2080ti
+#SBATCH -c8 --mem=24g
+#SBATCH --gres gpu:1 --constraint=2080ti
 
 module load nvidia/cuda-10.1
 module load nvidia/cudnn-v7.6.5.32-forcuda10.1
 
-#for i in 20210309-091413_brats20_3d_pretrained_new_lr_1_lr_0.1_epochs_60 20210309-133958_brats20_3d_pretrained_new_lr_1_lr_0.0001_epochs_60 20210309-170344_brats20_3d_pretrained_new_lr_1_lr_1e-05_epochs_60
-#for i in 20210309-100704_brats20_3d_pretrained_new_lr_1_lr_0.1_epochs_60 20210309-021115_brats20_3d_pretrained_new_1_lr_0.01_epochs_60
-for i in 20210309-133958_brats20_3d_pretrained_new_lr_1_lr_0.0001_epochs_60_50
+for i in 20210316-184209_brats20_3d_pretrained_originalconfig_1_lr_0.001_epochs_50_epochbatch_100_epoch
 do
 	python3 run_saved_models.py -model_name $i --batch_size 12 --patch_depth 24 --brats_test_year 20
 done
--- run_experiments_3d_2.sh
+++ run_experiments_3d_2.sh
@@ -2,17 +2,15 @@
 
 #SBATCH -p cs -A cs -q csug
 #SBATCH -c8 --mem=24g
-#SBATCH --gres gpu:1 --constraint=2080ti
+#SBATCH --gres gpu:2 --constraint=2080ti
 
 module load nvidia/cuda-10.1
 module load nvidia/cudnn-v7.6.5.32-forcuda10.1
 
 for i in 1
 do
-	for j in 50 75 
-	do
-		python3 train_jonas_net_batch.py -name brats20_3d_pretrained_trainbatchsize_$j --batch_size 12 --patch_depth 24 --brats_train_year 20 --brats_test_year 20 --learning_rate 0.0001 --epochs 50 --seed $i --training_batch_size $j --validation_batch_size $j		
-	done
+ 
+	python3 train_jonas_net_batch.py -name brats20_3d_pretrained_originalconfig_$i --batch_size 12 --patch_depth 24 --brats_train_year 20 --brats_test_year 20 --seed $i		
 
 done
 
