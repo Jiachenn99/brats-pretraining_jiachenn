66895459920419460ccc5987902dc496ee079836
Jiachenn Choong <choongjiachenn@gmail.com>
2021-04-27 19:52:48 +0100

    Updated gitignore for virtualenv; sh for training

--- run_experiments_3d.sh
+++ run_experiments_3d.sh
@@ -7,15 +7,7 @@
 module load nvidia/cuda-10.1
 module load nvidia/cudnn-v7.6.5.32-forcuda10.1
 
-for i in 6
+for i in 1
 do
-	python3 main.py -name brats_riccian_4_channels_weight_update_$i --batch_size 12 --patch_depth 24 --brats_train_year 20 --brats_test_year 20 --epochs 50 --num_channels 4 --seed $i		
+	python3 main.py -name brats_test_new_$i --batch_size 12 --training_max 20 --epochs 10 --num_channels 3 --seed $i		
 done
-
-
-# Brats18 data
-#python3 train_jonas_net_batch.py -name brats18_3d_pretrained_1  --batch_size 10 --patch_depth 24 --brats_train_year 18 --brats_test_year 18 --epochs 20 --multi_gpu --seed 1
-
-# Brats20 data
-#python3 train_jonas_net_batch.py -name brats20_3d_pretrained_new_$i  --batch_size 12 --patch_depth 24 --brats_train_year 20 --brats_test_year 20 --epochs 2 --seed $i
-
